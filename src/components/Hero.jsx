6f94c0ff4cfa7642754e2bef1dc87001ca1e6d62
camizalcman <camizalcman@gmail.com>
2026-08-01 11:46:48 -0300

    actualizacion de imagenes y productos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,15 @@ import { useState } from "react";
 import { MoveRight } from "lucide-react";
 
 const pizzas = [
-  { src: "/images/products/pizza.png", alt: "Pizza" },
-  { src: "/images/products/pizza1.png", alt: "Pizza 1" },
-  { src: "/images/products/pizza2.png", alt: "Pizza 2" },
-  { src: "/images/products/pizza3.png", alt: "Pizza 3" },
-  { src: "/images/products/pizza4.png", alt: "Pizza 4" },
+  { src: "/images/products/pizzaMuzza.png", alt: "Pizza de muzzarella" },
+  { src: "/images/products/pizzaRuculaChampi.png", alt: "Pizza de rucula" },
+  { src: "/images/products/pizzaCebolla.png", alt: "Pizza de cebolla" },
+  { src: "/images/products/pizzaPeperoni.png", alt: "Pizza con pepperoni" },
+  { src: "/images/products/pizzaPalmitos.png", alt: "Pizza con palmitos" },
+  { src: "/images/products/pizzaEspinaca.png", alt: "Pizza de espinaca" },
+  { src: "/images/products/pizzaHuevo.png", alt: "Pizza con huevo" },
+  { src: "/images/products/pizzaJamon.png", alt: "Pizza con jamon" },
+  { src: "/images/products/pizzaTomateChampi.png", alt: "Pizza de tomate" },
 ];
 
 const Hero = () => {
@@ -28,68 +32,74 @@ const [animating, setAnimating] = useState(false);
   };
 
   return (
-    <div className="flex items-center">
-      
-      
-      <div className="w-1/2 flex flex-col justify-center">
-        <h1 className="font-sora font-bold text-primary text-5xl">
-          Pizzas que se<br />hacen a tu manera.
-        </h1>
-        <h2 className="font-sora font-semibold text-primary text-2xl mt-4">
-          Elegí tu pizza favorita o armá la tuya desde cero
-        </h2>
+  <div className="flex flex-col lg:flex-row items-center">
+    
+    <div className="w-full lg:w-1/2 flex flex-col justify-center text-left">
+      <h1 className="font-sora font-bold text-primary text-4xl sm:text-4xl md:text-5xl">
+        Pizzas que se
+        <br />
+        hacen a tu manera.
+      </h1>
 
-        <div className="flex gap-4 mt-8">
-          <Link
-            href={`/product/6a378a5d20610ecf6443dfdf`}
-            className="bg-primary text-secondary rounded-full px-12 py-3 transition-all duration-300 hover:opacity-90 hover:scale-105"
-          >
-            Creá tu pizza
-          </Link>
+      <h2 className="font-sora font-semibold text-primary text-lg sm:text-xl md:text-2xl mt-4">
+        Elegí tu pizza favorita o armá la tuya desde cero
+      </h2>
 
-          <a href="#menu" className="inline-block border-2 border-primary text-primary rounded-full px-12 py-3 transition-all duration-300 hover:scale-105">Ver menú</a>
-        </div>
-      </div>
-
-      
-      <div className="w-1/2 flex flex-col items-center gap-2">
-        
-        <button
-          onClick={handleNext}
-          className="text-primary hover:text-primary-hover transition-colors text-3xl"
+      <div className="flex flex-col sm:flex-row gap-4 mt-8 justify-center lg:justify-start">
+        <Link
+          href={`/product/6a378a5d20610ecf6443dfdf`}
+          className="bg-primary text-secondary rounded-full px-8 md:px-12 py-3 transition-all duration-300 hover:opacity-90 hover:scale-105"
         >
-          <MoveRight className="w-6 h-6 text-primary" />
-        </button>
+          Creá tu pizza
+        </Link>
 
-        <div className="relative w-[550px] h-[550px]">
-          {pizzas.map((pizza, index) => (
-            <Image
-              key={index}
-              src={pizza.src}
-              alt={pizza.alt}
-              width={550}
-              height={550}
-              className="absolute top-0 left-0 transition-all duration-800"
-              style={{
-                opacity:
-                  index === current
-                    ? (animating ? 0 : 1)
-                    : (animating && index === (current + 1) % pizzas.length ? 1 : 0),
-
-                transform:
-                  index === current
-                    ? (animating ? "rotate(30deg)" : "rotate(0deg)")
-                    : (animating && index === (current + 1) % pizzas.length
-                        ? "rotate(0deg)"
-                        : "rotate(0deg)"),
-              }}
-            />
-          ))}
-        </div>
-
-
+        <a
+          href="#menu"
+          className="inline-block border-2 border-primary text-primary rounded-full px-8 md:px-12 py-3 transition-all duration-300 hover:scale-105"
+        >
+          Ver menú
+        </a>
       </div>
     </div>
+
+    <div className="w-full lg:w-1/2 flex flex-col items-center gap-2 mt-10 lg:mt-0">
+
+      <button
+        onClick={handleNext}
+        className="text-primary hover:text-primary-hover transition-colors text-2xl md:text-3xl"
+      >
+        <MoveRight className="w-6 h-6 text-primary" />
+      </button>
+
+      <div className="relative w-[280px] h-[280px] sm:w-[350px] sm:h-[350px] md:w-[450px] md:h-[450px] lg:w-[550px] lg:h-[550px]">
+        {pizzas.map((pizza, index) => (
+          <Image
+            key={index}
+            src={pizza.src}
+            alt={pizza.alt}
+            width={550}
+            height={550}
+            className="absolute top-0 left-0 w-full h-full object-contain transition-all duration-800"
+            style={{
+              opacity:
+                index === current
+                  ? (animating ? 0 : 1)
+                  : (animating && index === (current + 1) % pizzas.length ? 1 : 0),
+
+              transform:
+                index === current
+                  ? (animating ? "rotate(30deg)" : "rotate(0deg)")
+                  : (animating && index === (current + 1) % pizzas.length
+                      ? "rotate(0deg)"
+                      : "rotate(0deg)"),
+            }}
+          />
+        ))}
+      </div>
+
+    </div>
+  </div>
+
   )
 }
 
