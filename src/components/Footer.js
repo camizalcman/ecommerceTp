9709553bfbf9e8b70850ac0c1c6ea913525362b1
camizalcman <camizalcman@gmail.com>
2026-08-05 18:08:23 -0300

    modificacion en footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import Image from "next/image"
+import Link from "next/link"
+
+const links = [
+  { href: "/", label: "Home" },
+  { href: "/categories", label: "Categorias" },
+  { href: "/dashboard", label: "Dashboard" },
+];
 
 const Footer = () => {
 
     return (
-        <footer className='bg-primary text-secondary pt-24 pb-12 px-6'>
+        <footer className='bg-primary text-secondary pt-16 pb-12 px-6'>
 
             <div className='max-w-7xl mx-auto flex fflex-row justify-between items-start gap-12'>
 
@@ -12,34 +19,30 @@ const Footer = () => {
                     
                     
 
-                    <h2 className='font-medium text-[1.8em] sm:text-[2em] md:text-[2.5em] font-sora leading-none mt-2'>Crusta</h2>
-                    <Image className="mt-6"
+                    <Image 
                         src="/images/mascota.png"
                         alt="Mascota de la marca"
                         width={250}
                         height={250}
                         />
 
+                    
+                    <h2 className='font-medium text-[1.8em] sm:text-[4.4em] md:text-[2.5em] font-sora leading-none mt-2'>Crusta</h2>
+
                 </div>
 
                 
-                <nav>
-                    <ul className='flex flex-col items-start gap-4 text-neutral-300 text-sm font-medium'>
-                        
-                        <li>
-                            <a href="/#" className='hover:text-white transition-colors'>
-                                Categoría 1
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="/#" className='hover:text-white transition-colors'>
-                                Categoría 2
-                            </a>
-                        </li>
-
-                    </ul>
-                </nav>
+                <div className="flex flex-col gap-2">
+                    {links.map((link) => (
+                        <Link
+                        key={link.href}
+                        className="rounded-lg px-3 py-2 text-sm font-medium"
+                        href={link.href}
+                        >
+                        {link.label}
+                        </Link>
+                    ))}
+                    </div>
 
             </div>
 
